9ef5286f7080aa441b3ca159003abf8df3bfd969
Albania Musabeli <musabelialbania@gmail.com>
2025-03-29 12:06:49 -0300

    chore: Agregar comentarios a metodo de Servicios

--- src/main/java/com/musabeli/api_usuarios/services/UsuarioService.java
+++ src/main/java/com/musabeli/api_usuarios/services/UsuarioService.java
@@ -19,10 +19,20 @@ public class UsuarioService {
         this.usuariosList.add(new Usuario(4,"esteban98", "tjreo232435ldfj", "esteban@gmail.com", "CONDUCTOR"));
     }
 
+    /**
+     * Metodo que retorna una lista de Usuarios
+     * @return List<Usuario>
+     */
     public List<Usuario> getAllUsuarios(){
         return this.usuariosList;
     }
 
+    /**
+     * Busca un usuario por su ID.
+     *
+     * @param id el identificador del usuario a buscar.
+     * @return el objeto Usuario si se encuentra, o un objeto MensajeError si no existe.
+     */
     public Object getUsuarioById(int id){
         for (Usuario usuario : this.usuariosList){
             if (usuario.getId() == id) {
